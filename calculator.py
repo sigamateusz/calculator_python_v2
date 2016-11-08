c4ead914ab448543dee0968e98c3fcb34e62d436
Mateusz Siga <sigamateusz@gmail.com>
2016-11-08 14:48:11 +0100

    first commit

---
+++ calculator.py
@@ -0,0 +1,27 @@
+import os
+os.system('clear')
+
+def calculator(a , b , operation = '+', output_format = 'float'):
+    try:
+        a = float(a)
+        b = float(b)
+    except ValueError:
+        return 'You entered wrong values'
+    if operation == '+':
+        result = a + b
+    elif operation == '-':
+        result = a - b
+    elif operation == '*':
+        result = a * b
+    elif operation == '/':
+        result = a / b
+
+    if output_format == 'float':
+        return result
+    elif output_format == 'int':
+        return round(result) #
+    else:
+        return 'Wrong format of output'
+
+print(calculator(input('first number: '),input('second number: '),
+input('operation: '),input('output format: ')))
